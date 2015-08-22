e0d632d5aee69c48398ad528aad964fada3ee363
aude <aude.wiki@gmail.com>
2015-08-22 14:34:36 +0200

    Update namespace for EntityIdParser

--- src/EntityImporter.php
+++ src/EntityImporter.php
@@ -10,7 +10,7 @@ use Wikibase\DataModel\Entity\EntityIdValue;
 use Wikibase\DataModel\Entity\Item;
 use Wikibase\DataModel\Entity\ItemId;
 use Wikibase\DataModel\Serializers\StatementSerializer;
-use Wikibase\DataModel\Entity\BasicEntityIdParser;
+use Wikibase\DataModel\Services\EntityId\BasicEntityIdParser;
 use Wikibase\DataModel\Snak\PropertyValueSnak;
 use Wikibase\DataModel\Statement\StatementList;
 use Wikibase\Repo\Store\WikiPageEntityStore;

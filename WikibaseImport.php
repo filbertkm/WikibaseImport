06c1faab1e1c5b7e92c57c71a4cf18fc1a56d6dc
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2018-05-16 16:42:04 +0200

    Load composer autoloader from extension.json
    This ensures that loading the extension via
    
        wfLoadExtension( 'WikibaseImport' );
    
    instead of the PHP entry point also works.
    
    Fixes #32.

--- WikibaseImport.php
+++ WikibaseImport.php
@@ -1,10 +1,6 @@
 <?php
 
 if ( function_exists( 'wfLoadExtension' ) ) {
-	if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
-		require_once __DIR__ . '/vendor/autoload.php';
-	}
-
 	wfLoadExtension( 'WikibaseImport', __DIR__ . '/extension.json' );
 } else {
 	die( 'WikibaseImport requires MediaWiki 1.25+' );

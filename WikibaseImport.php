69d1f85dd0e9ea0024766cae0456ab5396c51dd0
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2018-05-16 16:46:06 +0200

    Add i18n globals to PHP entry point
    Even though the PHP entry point is deprecated, some part of its
    structure is still required for the mergeMessageFileList.php maintenance
    script. Ensure that this structure is present here.

--- WikibaseImport.php
+++ WikibaseImport.php
@@ -2,6 +2,9 @@
 
 if ( function_exists( 'wfLoadExtension' ) ) {
 	wfLoadExtension( 'WikibaseImport', __DIR__ . '/extension.json' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['WikibaseImport'] = __DIR__ . '/i18n';
+	$wgExtensionMessagesFiles['WikibaseImportAlias'] = __DIR__ . '/WikibaseImport.i18n.alias.php';
 	wfWarn(
 		'Deprecated PHP entry point used for WikibaseImport extension. ' .
 		'Please use wfLoadExtension instead, ' .

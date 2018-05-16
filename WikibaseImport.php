d9ff3bffd8f369dea291d2c205de6a30a177551d
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2018-05-16 16:44:10 +0200

    Deprecate PHP entry point

--- WikibaseImport.php
+++ WikibaseImport.php
@@ -2,6 +2,11 @@
 
 if ( function_exists( 'wfLoadExtension' ) ) {
 	wfLoadExtension( 'WikibaseImport', __DIR__ . '/extension.json' );
+	wfWarn(
+		'Deprecated PHP entry point used for WikibaseImport extension. ' .
+		'Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	);
 } else {
 	die( 'WikibaseImport requires MediaWiki 1.25+' );
 }
